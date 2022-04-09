4002c852b449fc7d5bfb4e277d686fc0e580486e
James Brett <jamesbrett3232@gmail.com>
2022-04-09 20:48:33 +0100

    added some scaffolding

---
+++ 445CalendarREPL/CalendarService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _445CalendarREPL
+{
+    public class CalendarService
+    {
+        public static FourFourFiveCalendar FourFourFiveCalendarForYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
